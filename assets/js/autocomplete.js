e7f5364f13cbadb01d6b639944311edf68d8f7c7
Selamawit926 <se.selamawit.elias@gmail.com>
2021-02-24 02:02:17 +0300

    A dropdown menu added for the seach bar

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -11,7 +11,13 @@ const createAutoComplete = ({
       <div class= "row  mx-auto">
       <input class="input" placeholder="Search..." />
       </div>
-      
+      <div class= "row">
+      <div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content results"></div>
+        </div>
+      </div>
+      </div>
       </div>
     `;
   
