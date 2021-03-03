0a7384f1b7e97201b3191a9e08159adecfbd3171
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-03-03 12:55:24 +0300

    controling the API to not crush

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -1,44 +1,44 @@
 const createAutoComplete = ({
-    root,
-    renderOption,
-    onOptionSelect,
-    inputValue,
-    fetchData
-  }) => {
-    root.innerHTML = `
-    <div class="container py-4 searchbar" >
-    
-      <div class= "row  mx-auto">
-      <input class="input" placeholder="Search..." />
-      </div>
-      <div class= "row">
-      <div class="dropdown">
-        <div class="dropdown-menu">
-          <div class="dropdown-content results"></div>
-        </div>
-      </div>
-      </div>
-      </div>
-    `;
+  root,
+  renderOption,
+  onOptionSelect,
+  inputValue,
+  fetchData
+}) => {
+  root.innerHTML = `
+  <div class="container py-4 searchbar" >
   
-const input = root.querySelector('input');
-const dropdown = root.querySelector('.dropdown');
-const resultsWrapper = root.querySelector('.results');
+    <div class= "row  mx-auto">
+    <input class="input" placeholder="Search..." />
+    </div>
+    <div class= "row">
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+    </div>
+    </div>
+  `;
 
-const onInput = async event => {
-const items = await fetchData(event.target.value);
+  const input = root.querySelector('input');
+  const dropdown = root.querySelector('.dropdown');
+  const resultsWrapper = root.querySelector('.results');
 
-  if (!items) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
+  const onInput = async event => {
+    const items = await fetchData(event.target.value);
 
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for (let item of items) {
-    const option = document.createElement('a');
-  
-    option.classList.add('dropdown-item');
+    if (!items) {
+      dropdown.classList.remove('is-active');
+      return;
+    }
+
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let item of items) {
+      const option = document.createElement('a');
+
+      option.classList.add('dropdown-item');
       option.innerHTML = renderOption(item);
       option.addEventListener('click', () => {
         dropdown.classList.remove('is-active');
@@ -47,18 +47,13 @@ const items = await fetchData(event.target.value);
       });
 
       resultsWrapper.appendChild(option);
+    }
+  };
+  input.addEventListener('input', debounce(onInput, 500));
 
-  }
-
+  document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+    }
+  });
 };
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', event => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
-
-});
-
-}
