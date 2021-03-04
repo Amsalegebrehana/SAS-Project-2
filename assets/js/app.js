c294126c2973cc644e52ce47d37a3833ada970c1
sitota49 <se.sitota.ezra@gmail.com>
2021-03-04 10:01:39 +0300

    initial request to API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,27 @@
 const autoCompleteConfig = {
     renderOption(book) {
 
-
+        const bookSrc = book.volumeInfo.imageLinks.smallThumbnail === 'N/A' ? '' : book.volumeInfo.imageLinks.smallThumbnail;
+        return `
+        <img src="${bookSrc}" />
+        ${book.volumeInfo.title} (${book.volumeInfo.publishedDate})
+      `;
     },
     inputValue(book) {
-
+        return book.volumeInfo.title;
     },
     async fetchData(searchTerm) {
+        const response = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+            params: {
+                q: searchTerm,
+                key: 'AIzaSyDoB7GTV_zWNG7Ad1P56fwweclsj7GsmWE'
+            }
+        });
 
+        if (response.data.Error) {
+            return [];
+        }
 
+        return response.data.items;
     }
 };
-
